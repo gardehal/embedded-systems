8bed1fa237ce24e89f8ca18e44f914bf037f1bc9
grdall <alex.thogar@gmail.com>
2020-03-11 04:41:58 +0100

    Update thermometer-activated-fan.ino

--- projects/thermometer-activated-fan/thermometer-activated-fan.ino
+++ projects/thermometer-activated-fan/thermometer-activated-fan.ino
@@ -1,6 +1,9 @@
-const int tempPin = A0;
+#include <dht.h>
 
-boolean engageFan = false;
+dht DHT;
+
+const int tempPin = 2;
+const int fanPin = 4;
 
 void setup() 
 {
@@ -9,6 +12,27 @@ void setup()
 
 void loop() 
 {
-  int tempReading = analogRead(tempPin);
-  delay(500);
+  int chk = DHT.read11(tempPin);
+  // chk fluxes between -1 (plausible readings) and -2 (-999 or 999). Ignore -2.
+  if(chk == -2)
+    return;
+    
+  Serial.print("Temperature = ");
+  Serial.println(DHT.temperature);
+  Serial.print("Humidity = ");
+  Serial.println(DHT.humidity);
+
+
+  // Relay to get 5v?
+  // Engage fan
+  if(DHT.temperature >  25)
+  {
+    digitalWrite(fanPin, HIGH);
+  }
+  else
+  {
+    digitalWrite(fanPin, LOW);
+  }
+  
+  delay(1000);
 }
