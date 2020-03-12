93250c0f2ae113de1fe99b8d5b834aaa63a2af99
grdall <alex.thogar@gmail.com>
2020-03-12 05:04:50 +0100

    Update thermometer-activated-fan.ino

--- projects/thermometer-activated-fan/thermometer-activated-fan.ino
+++ projects/thermometer-activated-fan/thermometer-activated-fan.ino
@@ -3,16 +3,18 @@
 dht DHT;
 
 const int tempPin = 2;
-const int fanPin = 4;
+const int fanRelayPin = 3;
 
 void setup() 
 {
+  pinMode(fanRelayPin, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() 
 {
   int chk = DHT.read11(tempPin);
+  
   // chk fluxes between -1 (plausible readings) and -2 (-999 or 999). Ignore -2.
   if(chk == -2)
     return;
@@ -22,16 +24,13 @@ void loop()
   Serial.print("Humidity = ");
   Serial.println(DHT.humidity);
 
-
-  // Relay to get 5v?
-  // Engage fan
   if(DHT.temperature >  25)
   {
-    digitalWrite(fanPin, HIGH);
+    digitalWrite(fanRelayPin, HIGH);
   }
   else
   {
-    digitalWrite(fanPin, LOW);
+    digitalWrite(fanRelayPin, LOW);
   }
   
   delay(1000);
