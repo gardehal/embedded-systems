4a1f8c002e6414abc1a8011bfb68b8948ab77022
grdall <alex.thogar@gmail.com>
2020-03-13 19:56:29 +0100

    temp loop and docs

--- projects/thermometer-activated-fan/thermometer-activated-fan.ino
+++ projects/thermometer-activated-fan/thermometer-activated-fan.ino
@@ -5,6 +5,8 @@ dht DHT;
 const int tempPin = 2;
 const int fanRelayPin = 3;
 
+const int collectionSet = 8;
+
 void setup() 
 {
   pinMode(fanRelayPin, OUTPUT);
@@ -13,25 +15,39 @@ void setup()
 
 void loop() 
 {
-  int chk = DHT.read11(tempPin);
-  
-  // chk fluxes between -1 (plausible readings) and -2 (-999 or 999). Ignore -2.
-  if(chk == -2)
-    return;
-    
-  Serial.print("Temperature = ");
-  Serial.println(DHT.temperature);
-  Serial.print("Humidity = ");
-  Serial.println(DHT.humidity);
+  float averageT = 0;
 
-  if(DHT.temperature >  25)
-  {
-    digitalWrite(fanRelayPin, HIGH);
-  }
-  else
-  {
-    digitalWrite(fanRelayPin, LOW);
-  }
+  for(int i = 0; i < collectionSet; i++)
+  { 
+    int chk = DHT.read11(tempPin);
   
-  delay(1000);
+    // chk fluxes between -1 (plausible readings) and -2 (-999 or 999). Ignore -2.
+    if(chk == -2)
+    {
+      i--;
+      continue;
+    }
+
+    float t = DHT.temperature;
+    float h = DHT.humidity;
+    
+    Serial.println(i);
+    Serial.print("Temperature = ");
+    Serial.println(t);
+    Serial.print("Humidity = ");
+    Serial.println(h);
+
+    averageT += t;
+  }
+
+  averageT = averageT / collectionSet;
+  
+  Serial.print("Average Temperature = ");
+  Serial.println(averageT);
+  Serial.println("--------------------------------------------");
+
+  if(averageT > 25)
+    digitalWrite(fanRelayPin, HIGH);
+  else
+    digitalWrite(fanRelayPin, LOW);
 }
--- projects/thermometer-activated-fan/thermometer-activated-fan.md
+++ projects/thermometer-activated-fan/thermometer-activated-fan.md
@@ -2,6 +2,10 @@
 
 A fan that is activated when a certain temperature and/or level of humidity is registered.
 
+I found that the DHT11 is unreliable, usually returning 14 or 28 (+/- 2), though it's likely not more than 25 degrees in the room at most. 
+For this reason I added a for loop to get an average of tempratures but it seems the inconsistency is from the sensor itself, not the wireing within the sensor or in the circuit. 
+I thought I noticed the sensor returned 14 whenever the fan turned on due to the fan cooling the sensor, but the drop is too much to be from the fan, as well as the fact that the fan does not point at the sensor suggests it's something else. It could be that the surge og power sent to the fan could be altering the signals sent to the Arduino board.
+
 [Code](./thermometer-activated-fan.ino)
 
 [Video](./thermometer-activated-fan.mp4)
