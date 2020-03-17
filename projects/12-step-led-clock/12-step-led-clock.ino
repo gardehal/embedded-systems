574fcc51262c3a23c9c5da32f95ce5f62c6ea7cf
grdall <alex.thogar@gmail.com>
2020-03-17 06:09:57 +0100

    experementing with  Charlieplexing
    https://youtu.be/Bx5GLyJSWPk

--- projects/12-step-led-clock/12-step-led-clock.ino
+++ projects/12-step-led-clock/12-step-led-clock.ino
@@ -1,3 +1,17 @@
+// Pins for charlieplexing
+const int digitalTwo = 2;
+const int digitalThree = 3;
+const int digitalFour = 4;
+const int digitalFive = 5;
+const int digitalSix = 6;
+const int digitalSeven = 7;
+const int digitalEight = 8;
+const int digitalNine = 9;
+const int digitalTen = 10;
+const int digitalEleven = 11;
+const int digitalTwelve = 12;
+const int digitalThirteen = 13;
+
 void setup() 
 {
   Serial.begin(9600);
@@ -5,4 +19,50 @@ void setup()
 
 void loop() 
 {
+  reset();
+  lightTopLeft();
+}
+
+void reset()
+{
+  for(int i = 2; i < 14; i++)
+  {
+    pinMode(i, OUTPUT);
+  }
+}
+
+void lightTopLeft()
+{
+  digitalWrite(digitalThirteen, HIGH);
+  digitalWrite(digitalTwelve, LOW);
+}
+
+void lightTopRight()
+{
+  digitalWrite(digitalEleven, HIGH);
+  digitalWrite(digitalThirteen, LOW);
+}
+
+void lightMiddleLeft()
+{
+  digitalWrite(digitalThirteen, HIGH);
+  digitalWrite(digitalTwelve, LOW);
+}
+
+void lightMiddleRight()
+{
+  digitalWrite(digitalTwelve, HIGH);
+  digitalWrite(digitalThirteen, LOW);
+}
+
+void lightBottomLeft()
+{
+  digitalWrite(digitalThirteen, HIGH);
+  digitalWrite(digitalEleven, LOW);
+}
+
+void lightBottomRight()
+{
+  digitalWrite(digitalTwelve, HIGH);
+  digitalWrite(digitalThirteen, LOW);
 }
