561bec28c438dccd3e5c2013795b2e0c64ac2ace
grdall <alex.thogar@gmail.com>
2020-03-22 06:14:33 +0100

    working on logic for clock

--- projects/12-step-led-clock/12-step-led-clock.ino
+++ projects/12-step-led-clock/12-step-led-clock.ino
@@ -1,3 +1,7 @@
+
+#include <Time.h>
+
+#define TIME_HEADER 255
 // Pins for charlieplexing
 const int digitalTwo = 2;
 const int digitalThree = 3;
@@ -12,88 +16,116 @@ const int digitalEleven = 11;
 const int digitalTwelve = 12;
 const int digitalThirteen = 13;
 
+const int delayMinute = 60000; // 60 000 milliseconds = 1 minute
+
 void setup() 
 {  
   Serial.begin(9600);
-}
 
+  reset();
+}
 void loop() 
 {
-  lightTopLeft();
-  delay(1000);
-  lightTopRight();
-  delay(1000);
-  lightMiddleLeft();
-  delay(1000);
-  lightMiddleRight();
-  delay(1000);
-  lightBottomLeft();
-  delay(1000);
-  lightBottomRight();
-  delay(1000);
+  // Hours = (DateTime.Hour,DEC);
+  // Minutes = DateTime.Minute;
+  // Seconds = DateTime.Second;
+  //Serial.print((DateTime.Hour,DEC));
+  
+  blinkAll();
+
+  // Get time of day?
+  // set hour to current hour
+  // set minutes to closes minute in 5 minute steps, set difference as delay time
+  // enter infinite loop where every 5 minutes, increment minute led steps, every hour (12 minute steps) increment hours and set minutes back to 0
 }
 
-void test()
+
+
+void blinkAll()
 {
-  test();
+  // digitalTwo
+  // digitalThree
+  // digitalFour
+  // digitalFive
+  // digitalSix
+  // digitalSeven
+  // digitalEight
+  // digitalNine
+  // digitalTen
   // digitalEleven
   // digitalTwelve
   // digitalThirteen
-  pinMode(digitalThirteen, INPUT);
-  pinMode(digitalTwelve, OUTPUT);
-  pinMode(digitalEleven, OUTPUT);
-  digitalWrite(digitalTwelve, HIGH);
-  digitalWrite(digitalEleven, LOW);
+  
+  // Top left cluster
+  delay(1000);
+  lightLed(digitalEight, digitalTen); // Grid 0, 0
+  delay(1000);
+  lightLed(digitalTen, digitalEight); // Grid 0, 1
+  delay(1000);
+  lightLed(digitalNine, digitalTen); // Grid 1, 0
+  delay(1000);
+  lightLed(digitalEight, digitalNine); // Grid 1, 1
+  delay(1000);
+  lightLed(digitalTen, digitalNine); // Grid 2, 0
+  delay(1000);
+  lightLed(digitalNine, digitalEight); // Grid 2, 1
+
+  // Bottom left cluster
+  delay(1000);
+  lightLed(digitalThirteen, digitalTwelve); // Grid 3, 0
+  delay(1000);
+  lightLed(digitalTwelve, digitalEleven); // Grid 3, 1
+  delay(1000);
+  lightLed(digitalTwelve, digitalThirteen); // Grid 4, 0
+  delay(1000);
+  lightLed(digitalEleven, digitalTwelve); // Grid 4, 1
+  delay(1000);
+  lightLed(digitalEleven, digitalThirteen); // Grid 5, 0
+  delay(1000);
+  lightLed(digitalThirteen, digitalEleven); // Grid 5, 1
+
+  // Top right cluster
+  delay(1000);
+  lightLed(digitalFive, digitalSeven); // Grid 0, 2
+  delay(1000);
+  lightLed(digitalSeven, digitalFive); // Grid 0, 3
+  delay(1000);
+  lightLed(digitalSix, digitalSeven); // Grid 1, 2
+  delay(1000);
+  lightLed(digitalFive, digitalSix); // Grid 1, 3
+  delay(1000);
+  lightLed(digitalSeven, digitalSix); // Grid 2, 2
+  delay(1000);
+  lightLed(digitalSix, digitalFive); // Grid 2, 3
+
+  // Bottom rightt cluster
+  delay(1000);
+  lightLed(digitalFour, digitalThree); // Grid 3, 2
+  delay(1000);
+  lightLed(digitalThree, digitalTwo); // Grid 3, 3
+  delay(1000);
+  lightLed(digitalThree, digitalFour); // Grid 4, 2
+  delay(1000);
+  lightLed(digitalTwo, digitalThree); // Grid 4, 3
+  delay(1000);
+  lightLed(digitalTwo, digitalFour); // Grid 5, 2
+  delay(1000);
+  lightLed(digitalFour, digitalTwo); // Grid 5, 3
 }
 
-void lightTopLeft()
+void reset()
 {
-  pinMode(digitalTwelve, INPUT);
-  pinMode(digitalEleven, OUTPUT);
-  pinMode(digitalThirteen, OUTPUT);
-  digitalWrite(digitalEleven, HIGH);
-  digitalWrite(digitalThirteen, LOW);
+  for(int i = 0; i < 14; i++)
+  {
+    pinMode(i, INPUT);
+  }
 }
 
-void lightTopRight()
+void lightLed(int firstPin, int secondPin)
 {
-  pinMode(digitalTwelve, INPUT);
-  digitalWrite(digitalThirteen, HIGH);
-  digitalWrite(digitalEleven, LOW);
-}
-
-void lightMiddleLeft()
-{
-  pinMode(digitalEleven, INPUT);
-  pinMode(digitalTwelve, OUTPUT);
-  pinMode(digitalThirteen, OUTPUT);
-  digitalWrite(digitalTwelve, HIGH);
-  digitalWrite(digitalThirteen, LOW);
-}
-
-void lightMiddleRight()
-{
-  pinMode(digitalThirteen, INPUT);
-  pinMode(digitalEleven, OUTPUT);
-  pinMode(digitalTwelve, OUTPUT);
-  digitalWrite(digitalEleven, HIGH);
-  digitalWrite(digitalTwelve, LOW);
-}
-
-void lightBottomLeft()
-{
-  pinMode(digitalEleven, INPUT);
-  pinMode(digitalThirteen, OUTPUT);
-  pinMode(digitalTwelve, OUTPUT);
-  digitalWrite(digitalThirteen, HIGH);
-  digitalWrite(digitalTwelve, LOW);
-}
-
-void lightBottomRight()
-{
-  pinMode(digitalThirteen, INPUT);
-  pinMode(digitalTwelve, OUTPUT);
-  pinMode(digitalEleven, OUTPUT);
-  digitalWrite(digitalTwelve, HIGH);
-  digitalWrite(digitalEleven, LOW);
+  reset();
+  pinMode(firstPin, OUTPUT);
+  pinMode(secondPin, OUTPUT);
+  digitalWrite(firstPin, HIGH);
+  digitalWrite(secondPin, LOW);
 }
--- projects/12-step-led-clock/12-step-led-clock.md
+++ projects/12-step-led-clock/12-step-led-clock.md
@@ -5,6 +5,10 @@ The two white leds at the top represent AM/PM. AM left, PM right.
  
 I realized after the project was initialized that the board only have 13 digital outputs and 6 analog ones, which is not enough to power all the 26 leds separately. 
 Maybe there are some library or trickery one can do use, otherwise I will just develop the code and demonstrate it in two parts.
+
+// Edits:
+Had to charlieplex, 4 sets of 6 due to complexity and hardware limits, had to change layout from 2 cols of 12 to 4 of 6
+
  
 [Code](./12-step-led-clock.ino)
  
