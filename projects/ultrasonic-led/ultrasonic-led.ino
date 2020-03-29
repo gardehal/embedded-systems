42f965830ca13d6d8212bd8aa95a0c49e9d54cec
grdall <alex.thogar@gmail.com>
2020-03-29 07:11:49 +0200

    using some default code for adjusting the blinking

--- projects/ultrasonic-led/ultrasonic-led.ino
+++ projects/ultrasonic-led/ultrasonic-led.ino
@@ -1,16 +1,47 @@
 
-const int us1 = 8;
-const int us2 = 9;
-const int us3 = 10;
+const int trigPin = 9;
+const int echoPin = 10;
 const int ledRedPin = 11;
-const int ledBluePin = 12;
-const int ledGreenPin = 13;
+const int ledGreenPin = 12;
+const int ledBluePin = 13;
+
+long duration;
+int distance;
 
 void setup() 
 {
   Serial.begin(9600);
+  
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  pinMode(ledRedPin, OUTPUT);
+  pinMode(ledGreenPin, OUTPUT);
+  pinMode(ledBluePin, OUTPUT);
 }
 
 void loop() 
 {
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  
+  // Calculating the distance in centimeters
+  distance = duration*0.034/2;
+
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance);
+
+  if(distance < 100)
+  {
+    digitalWrite(ledBluePin, HIGH);
+    delay(distance * 10);
+    digitalWrite(ledBluePin, LOW);
+    delay(distance * 10);
+  }
 }
--- projects/ultrasonic-led/ultrasonic-led.md
+++ projects/ultrasonic-led/ultrasonic-led.md
@@ -2,6 +2,8 @@
 
 A project for experimenting with the ultrasonic proximity sensor. The sensor changes the colour of a RGB led based on proximity. 
 
+Max distance appears to be 35 centimeters.
+
 [Code](./ultrasonic-led.ino)
 
 [Video](./ultrasonic-led.mp4)
