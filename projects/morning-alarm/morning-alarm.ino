d07dbb8ca824ff465db29b330cba3a55229c35d6
grdall <alex.thogar@gmail.com>
2020-04-03 05:24:46 +0200

    alarm main logic done

--- projects/morning-alarm/morning-alarm.ino
+++ projects/morning-alarm/morning-alarm.ino
@@ -4,11 +4,10 @@ const int tiltSwitchPin = A1;
 const int passiveBuzzerPin = 12;
 const int activeBuzzerPin = 13;
 
-const long minute = 60000; // 60 seconds
-const int delayMinutes = 1;
+const long delayTime = 60000; // 60 seconds
 
-int alarm = 0600;
-int currentTime = 0;
+long alarm = 1;
+long currentTime = 0;
 boolean enableAlarm = false;
 
 void setup() 
@@ -25,21 +24,35 @@ void setup()
 }
 
 void loop() 
-{
-  if(currentTime == alarm - 1)
+{  
+  Serial.println("currentTime ");
+  Serial.println(currentTime);
+  
+  Serial.println("alarm ");
+  Serial.println(alarm);
+  
+  Serial.println("enableAlarm ");
+  Serial.println(enableAlarm);
+  
+  Serial.println("------------");
+  
+  if(currentTime == (alarm - 1) && !enableAlarm)
   { 
-    delay(minute); // Trigger on minute before alarm is set, wait one minute and then trigger to avoid alarm trigger, then turned of before time updates, causing the alarm to trigger again.
+    Serial.println("currentTime == alarm - 1");
+    delay(60000); // Trigger on minute before alarm is set, wait one minute and then trigger to avoid alarm trigger, then turned of before time updates, causing the alarm to trigger again.
     enableAlarm = true;
   }
 
   if(enableAlarm)
   {
+    Serial.println("enableAlarm true");
     soundAlarm(); // Causes alarm sound
     checkDisableAlarm(); // Will disable alarm if requirements are met
     return;
   }
   
-  delay(minute * delayMinutes);
+  Serial.println("delayTime");
+  delay(delayTime);
   
   // Synchronize time // Can be done with a counter so it's only done every x minutes in case of internet usage to save some bandwidth
   currentTime = syncTime();
@@ -49,15 +62,15 @@ void soundAlarm()
 {
   // analogWrite
   // digitalWrite
-  digitalWrite(passiveBuzzerPin, HIGH);
+  digitalWrite(activeBuzzerPin, HIGH);
   delay(500);
-  digitalWrite(passiveBuzzerPin, LOW);
+  digitalWrite(activeBuzzerPin, LOW);
   delay(500);
 }
 
-int syncTime()
+long syncTime()
 {
-  
+  return 0;
 }
 
 void checkDisableAlarm()
@@ -68,9 +81,9 @@ void checkDisableAlarm()
   Serial.print("Photoresistor value: ");
   Serial.println(pResistorValue);
   Serial.print("Tilt ball switch value: ");
-  Serial.println(tiltSwitchPin);
+  Serial.println(tiltSwitchValue);
   
   // Disable alarm
-  if(pResistorValue > 1000 && tiltSwitchPin > 500)
+  if(pResistorValue > 1000 && tiltSwitchValue > 500)
     enableAlarm = false;
 }
--- projects/morning-alarm/morning-alarm.md
+++ projects/morning-alarm/morning-alarm.md
@@ -1,6 +1,8 @@
 # Morning Alarm
  
 An alarm that can be used to wake one up in the morning. This is a project focused on the tilt ball switch, photoresistor, active and passive buzzer. It is not meant to be very accurate, but rather to deploy modules in a real life situation. One could convert this to a working alarm, but it is suggested to include a real time clock or some other way to synchronize the projects internal variables to accurate real time.
+
+The combination of light and movement surely is the best way to wake someone up.
  
 [Code](./morning-alarm.ino)
  
