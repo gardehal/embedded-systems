92042ae3a64c18bc281b4d8642c26a4f30d18865
grdAll <alex.thogar@gmail.com>
2021-05-08 10:09:44 +0200

    Corrected some issues and removed testing code

--- projects/buzzer-music/buzzer-music.ino
+++ projects/buzzer-music/buzzer-music.ino
@@ -4,6 +4,7 @@
 
 // Melodies
 #include "melodies/example.h"
+#include "melodies/twinkle-twinkle-little-star.h"
 // #include "melodies/all-star.h"
 
 const int speakerPin = 13;
@@ -19,26 +20,23 @@ void setup()
 void serialEvent() 
 {
     // tempo|note1@beat1|note2@beat2|note3@beat3|note4@beat4... tempo, beats, and notes are 4 char long ints
-    // 0114|0466@0228|0466@0456|0415@0228|0370@0228
+    // 0114|0370@0004|0554@0002|0466@0004|0466@0002
+    String in1 = "0114|0370@0004|0554@0002|0466@0004|0466@0002";
     String in = Serial.readString();
     int tempo = in.substring(0, 4).toInt();
     String consumeString = in.substring(5);
     int len = (in.length() + 1) / 10; // 0000|.. ends with 0000, add one to get a length that is divisible with 5
     int notes[len];
     int beats[len];
-
+    
     for (int i = 0; i < len; i++)
     {
-      notes[i] = consumeString.substring(0, 4).toInt();
+      notes[i] = consumeString.substring(1, 4).toInt();
       beats[i] = consumeString.substring(5, 9).toInt();
       consumeString = consumeString.substring(10);
     }
-
-    delay(1000);
-    int notes1[4] = {466, 466, 415, 370};
-    int beats1[4] = {228, 456, 228, 228};
     
-    play(4, 114, notes1, beats1);
+    play(len, tempo, notes, beats);
 }
 
 void loop() 
@@ -89,6 +87,6 @@ void play(int len, int tempo, int* notes, int* beats)
     tone(speakerPin, notes[i], beats[i] * tempo);
 
     // pause between notes
-    delay(tempo / 2); 
+    delay(tempo * 4); 
   }
 }
