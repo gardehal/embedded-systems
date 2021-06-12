38849bbf6b174701857df1b21545f21c1b891430
grdAll <alex.thogar@gmail.com>
2021-06-12 10:59:38 +0200

    proto IR code to replace joystick since it's clunky and potentiometer  flickers servo. IR receiver burnt out, keep using joystick for now

--- projects/arduino-arm/arduino-arm.ino
+++ projects/arduino-arm/arduino-arm.ino
@@ -1,7 +1,32 @@
+#include <IRremote.h>
 #include <Servo.h>
 #include <ResponsiveAnalogRead.h>
 
-// https://create.arduino.cc/projecthub/MisterBotBreak/how-to-use-a-joystick-with-serial-monitor-1f04f0
+const long keyPower = 16753245;
+const long keyVolPluss = 16736925;
+const long keyFunc = 16769565;
+const long keyLeft = 16720605;
+const long keyPlay = 16712445;
+const long keyRight = 16761405;
+const long keyDown = 16769055;
+const long keyVolMinus = 16754775;
+const long keyUp = 16748655;
+const long key0 = 16738455;
+const long keyEq = 16750695;
+const long keySt = 16756815;
+const long key1 = 16724175;
+const long key2 = 16718055;
+const long key3 = 16743045;
+const long key4 = 16716015;
+const long key5 = 16726215;
+const long key6 = 16734885;
+const long key7 = 16728765;
+const long key8 = 16730805;
+const long key9 = 16732845;
+
+const int irPin = 2;
+IRrecv ir(irPin);
+decode_results results;
 
 // Joystick for arm base and and first joint
 // Potentiometer for second joint
@@ -32,14 +57,11 @@ int servo2Pos = 90;
 int servo3Pos = 90;
 int servo4Pos = 0;
 
-ResponsiveAnalogRead analog(potPin, false);
 void setup()
 {
   Serial.begin(9600);
-
-  pinMode(VRx, INPUT);
-  pinMode(VRy, INPUT);
-  pinMode(SW, INPUT);
+  
+  ir.enableIRIn();
 
   servo1.attach(servo1Pin);
   servo2.attach(servo2Pin);
@@ -53,25 +75,84 @@ void setup()
 
 void loop()
 {
-  xPosition = analogRead(VRx);
-  yPosition = analogRead(VRy);
-  //zPosition = analogRead(potPin);
-  analog.update();
-  SW_state = digitalRead(SW);
-  mapX = map(xPosition, 0, 1023, 0, 180);
-  mapY = map(yPosition, 0, 1023, 0, 180);
-  mapZ = map(analog.getValue(), 0, 1023, 0, 180);
+  if(ir.decode())
+  {
+    switch(results.value)
+    {
+      case keyPower:
+        Serial.print("Power");
+        break;
+      case keyVolPluss:
+        Serial.print("VOL+");
+        servo2Pos += 4;
+        break;
+      case keyFunc:
+        Serial.print("FUNC/STOP");
+        break;
+      case keyLeft:
+        Serial.print("Left");
+        servo1Pos += 4;
+        break;
+      case keyPlay:
+        Serial.print("Play/Pause");
+        break;
+      case keyRight:
+        Serial.print("Right");
+        servo1Pos -= 4;
+        break;
+      case keyDown:
+        Serial.print("Down");
+        break;
+      case keyVolMinus:
+        Serial.print("VOL-");
+        servo2Pos -= 4;
+        break;
+      case keyUp:
+        Serial.print("Up");
+        break;
+      case key0:
+        Serial.print("0");
+        break;
+      case keyEq:
+        Serial.print("EQ");
+        break;
+      case keySt:
+        Serial.print("ST/REPT");
+        break;
+      case key1:
+        Serial.print("1");
+        break;
+      case key2:
+        Serial.print("2");
+        break;
+      case key3:
+        Serial.print("3");
+        break;
+      case key4:
+        Serial.print("4");
+        break;
+      case key5:
+        Serial.print("5");
+        break;
+      case key6:
+        Serial.print("6");
+        break;
+      case key7:
+        Serial.print("7");
+        break;
+      case key8:
+        Serial.print("8");
+        break;
+      case key9:
+        Serial.print("9");
+        break;
+      default:
+        Serial.print(" - value: ");
+        Serial.println(results.value);
+    }
 
-  Serial.print("X: ");
-  Serial.print(mapX);
-  Serial.print(" | Y: ");
-  Serial.print(mapY);
-  Serial.print(" | Button: ");
-  Serial.println(SW_state);
-
-  servo1Pos = mapY;
-  servo2Pos = mapX;
-  servo3Pos = mapZ;
+      ir.resume(); 
+  }
 
   servo1.write(servo1Pos);
   servo2.write(servo2Pos);
