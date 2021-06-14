fa8e81f1c1e375ec58e7ef73c58764269bf0f26f
grdAll <alex.thogar@gmail.com>
2021-06-14 23:00:54 +0200

    Update arduino-arm.ino

--- projects/arduino-arm/arduino-arm.ino
+++ projects/arduino-arm/arduino-arm.ino
@@ -8,18 +8,9 @@ const int VRy = A1;
 const int SW = 2;
 const int potPin = A2;
 
-int xPosition = 0;
-int yPosition = 0;
-int zPosition = 0;
-int SW_state = 0;
-int mapX = 0;
-int mapY = 0;
-int mapZ = 0;
-int mapG = 0;
-
 const int initialState = 90;
 const int padding = 15;
-const int moveSteps = 2;
+
 const int servo1Pin = 11;
 const int servo2Pin = 10;
 const int servo3Pin = 9;
@@ -39,6 +30,7 @@ void setup()
   servo2.attach(servo2Pin);
   servo3.attach(servo3Pin);
   servo4.attach(servo4Pin);
+  pinMode(SW, INPUT); 
   
   servo1.write(servo1Pos);
   servo2.write(servo2Pos);
@@ -50,15 +42,15 @@ void setup()
 
 void loop()
 {  
-  xPosition = analogRead(VRx);
-  yPosition = analogRead(VRy);
-  zPosition = 0;//analogRead(potPin);
-  SW_state = 0;//digitalRead(SW);
+  int xPosition = analogRead(VRx);
+  int yPosition = analogRead(VRy);
+  int zPosition = analogRead(potPin);
+  int SW_state = digitalRead(SW);
   
-  mapX = map(xPosition, 0, 1023, 0, 180);
-  mapY = map(yPosition, 0, 1023, 0, 180);
-  mapZ = map(zPosition, 0, 1023, 0, 180);
-  mapG = map(SW_state, 0, 1, 0, 1);
+  int mapX = map(xPosition, 0, 1023, -90, 90);
+  int mapY = map(yPosition, 0, 1023, -90, 90);
+  int mapZ = map(zPosition, 0, 1023, -90, 90);
+  int mapG = map(SW_state, 0, 1, 0, 1);
   
   Serial.print("X: ");
   Serial.print(mapX);
@@ -71,54 +63,47 @@ void loop()
   Serial.println("");
 
   // Update servos based on current position +/- joystick input 
-  if(mapX > (initialState + padding) && servo1Pos < (180 - moveSteps - 5))
+  if(abs(mapX) > padding)
   {
-    servo1Pos += moveSteps; 
+    servo1Pos = calcMove(mapX, servo1Pos);
     servo1.write(servo1Pos);
-    Serial.print("servo1Pos: ");
-    Serial.println(servo1Pos);
-  }
-  else if(mapX < (initialState - padding) && servo1Pos > (0 + moveSteps + 5))
-  {
-    servo1Pos -= moveSteps; 
-    servo1.write(servo1Pos);
-    Serial.print("servo1Pos: ");
-    Serial.println(servo1Pos);
-  }
     
-  if(mapY > (initialState + padding) && servo2Pos < (180 - moveSteps - 5))
-  {
-    servo2Pos += moveSteps; 
-    servo2.write(servo2Pos);
-    Serial.print("servo2Pos: ");
-    Serial.println(servo2Pos);
+    Serial.print("servo1Pos: ");
+    Serial.println(servo1Pos);
   }
-  else if(mapY < (initialState - padding) && servo2Pos < (0 + moveSteps + 5))
-  {
-    servo2Pos -= moveSteps; 
-    servo2.write(servo2Pos);
-    Serial.print("servo2Pos: ");
-    Serial.println(servo2Pos);
-  }
-   
-  if(mapZ > (initialState + padding) && servo3Pos < (180 - moveSteps - 5))
-  {
-    servo3Pos += moveSteps; 
-    servo3.write(servo3Pos);
-  }
-  else if(mapZ < (initialState - padding) && servo3Pos < (0 + moveSteps + 5))
-  {
-    servo3Pos -= moveSteps; 
-    servo3.write(servo3Pos);
-  }
-
-  // TODO grabber
-  //if(!SW_state)
-    //servo4Pos = 0;
-  //else
-    //servo4Pos = 180;
   
-  //servo4.write(servo4Pos);
+  if(abs(mapY) > padding)
+  {
+    servo2Pos = calcMove(mapY, servo2Pos);
+    servo2.write(servo2Pos);
+    
+    Serial.print("servo2Pos: ");
+    Serial.println(servo2Pos);
+  }
+  
+  if(false && abs(mapZ) > padding)
+  {
+    servo3Pos = calcMove(mapZ, servo3Pos);
+    servo3.write(servo3Pos);
+    
+    Serial.print("servo3Pos: ");
+    Serial.println(servo3Pos);
+  }
+  
+  if(false && !SW_state)
+  {
+    //servo4.write(servo4Pos);
+  }
 
   delay(100);
+}
+
+int calcMove(int mapVar, int current)
+{
+    int steps = abs(mapVar)/10;
+    
+    if(mapVar > 0 && current < (180 - steps))
+      return (current += steps); 
+    else if(mapVar < 0 && current > (0 + steps))
+      return (current -= steps); 
 }
