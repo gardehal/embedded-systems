ac3e9c37301c459abb5b90a74d461b7c93602019
grdAll <alex.thogar@gmail.com>
2021-07-11 00:02:29 +0200

    Rewrote code to use a basic version with IR

--- projects/arduino-arm/arduino-arm.ino
+++ projects/arduino-arm/arduino-arm.ino
@@ -1,109 +1,159 @@
-#include <Servo.h>
+#include "Servo.h"
+#include "IRremote.h"
 
-// Joystick for arm base and and first joint
-// Potentiometer for second joint
-// Button for fingers
-const int VRx = A0;
-const int VRy = A1;
-const int SW = 2;
-const int potPin = A2;
+// Elegoo remote
+#include "ElegooIrRemote.h"
+ElegooIrRemote irRemote;
 
 const int initialState = 90;
 const int padding = 15;
 
-const int servo1Pin = 11;
-const int servo2Pin = 10;
-const int servo3Pin = 9;
-const int servo4Pin = 6;
+const int irRecPin = 2;
+const int servo1Pin = 12;
+const int servo2Pin = 11;
+const int servo3Pin = 10;
+const int servo4Pin = 9;
+const int servo5Pin = 8;
 Servo servo1;
 Servo servo2;
 Servo servo3;
 Servo servo4;
+Servo servo5;
 int servo1Pos = initialState;
 int servo2Pos = initialState;
 int servo3Pos = initialState;
 int servo4Pos = initialState;
+int servo5Pos = initialState;
 
 void setup()
 {
+  IrReceiver.begin(irRecPin);
+  
   servo1.attach(servo1Pin);
   servo2.attach(servo2Pin);
   servo3.attach(servo3Pin);
   servo4.attach(servo4Pin);
-  pinMode(SW, INPUT); 
+  servo5.attach(servo5Pin);
   
   servo1.write(servo1Pos);
   servo2.write(servo2Pos);
   servo3.write(servo3Pos);
   servo4.write(servo4Pos);
+  servo5.write(servo5Pos);
   
   Serial.begin(9600);
 }
 
+  
+  // left/right = irRemote.key4, irRemote.key6
+  // humerus up/down = irRemote.key1, irRemote.key7
+  // radius up/down = irRemote.key2, irRemote.key8
+  // hand up/down = irRemote.key3, irRemote.key9
+  // fingers close/open = irRemote.key5, irRemote.key0
+
+  // emote wave = ?
+  // emote show range = playPause
+
 void loop()
-{  
-  int xPosition = analogRead(VRx);
-  int yPosition = analogRead(VRy);
-  int zPosition = analogRead(potPin);
-  int SW_state = digitalRead(SW);
+{ 
+  if(IrReceiver.decode())
+  {    
+    switch(IrReceiver.decodedIRData.command)
+    {
+      case irRemote.keyPower:
+        Serial.println("Power");
+        break;
+      case irRemote.keyVolumePlus:
+        Serial.println("VOL+");
+        break;
+      case irRemote.keyFuncStop:
+        Serial.println("FUNC/STOP");
+        break;
+      case irRemote.keyBack:
+        Serial.println("Back");
+        break;
+      case irRemote.keyPlayPause:
+        Serial.println("Play/Pause");
+        break;
+      case irRemote.keyForward:
+        Serial.println("Forward");
+        break;
+      case irRemote.keyDown:
+        Serial.println("Down");
+        break;
+      case irRemote.keyVolumeMinus:
+        Serial.println("VOL-");
+        break;
+      case irRemote.keyUp:
+        Serial.println("Up");
+        break;
+      case irRemote.key0:
+        Serial.println("0");
+        break;
+      case irRemote.keyEq:
+        Serial.println("EQ");
+        break;
+      case irRemote.keyStRept:
+        Serial.println("ST/REPT");
+        break;
+      case irRemote.key1:
+        Serial.println("1");
+        servo2Pos = calcMove(10, servo2Pos);
+        servo2.write(servo2Pos);
+        break;
+      case irRemote.key2:
+        Serial.println("2");
+        servo3Pos = calcMove(10, servo3Pos);
+        servo3.write(servo3Pos);
+        break;
+      case irRemote.key3:
+        Serial.println("3");
+        servo4Pos = calcMove(10, servo4Pos);
+        servo4.write(servo4Pos);
+        break;
+      case irRemote.key4:
+        Serial.println("4");
+        servo1Pos = calcMove(10, servo1Pos);
+        servo1.write(servo1Pos);
+        break;
+      case irRemote.key5:
+        Serial.println("5");
+        break;
+      case irRemote.key6:
+        Serial.println("6");
+        servo1Pos = calcMove(-10, servo1Pos);
+        servo1.write(servo1Pos);
+        break;
+      case irRemote.key7:
+        Serial.println("7");
+        servo2Pos = calcMove(-10, servo2Pos);
+        servo2.write(servo2Pos);
+        break;
+      case irRemote.key8:
+        Serial.println("8");
+        servo3Pos = calcMove(-10, servo3Pos);
+        servo3.write(servo3Pos);
+        break;
+      case irRemote.key9:
+        Serial.println("9");
+        servo4Pos = calcMove(-10, servo4Pos);
+        servo4.write(servo4Pos);
+        break;
+      default:
+        Serial.print(" - value: ");
+        Serial.println(IrReceiver.decodedIRData.command);
+    }
   
-  int mapX = map(xPosition, 0, 1023, -90, 90);
-  int mapY = map(yPosition, 0, 1023, -90, 90);
-  int mapZ = map(zPosition, 0, 1023, -90, 90);
-  int mapG = map(SW_state, 0, 1, 0, 1);
-  
-  Serial.print("X: ");
-  Serial.print(mapX);
-  Serial.print(" | Y: ");
-  Serial.print(mapY);
-  Serial.print(" | Z: ");
-  Serial.print(mapZ);
-  Serial.print(" | G: ");
-  Serial.print(mapG);
-  Serial.println("");
-
-  // Update servos based on current position +/- joystick input 
-  if(abs(mapX) > padding)
-  {
-    servo1Pos = calcMove(mapX, servo1Pos);
-    servo1.write(servo1Pos);
-    
-    Serial.print("servo1Pos: ");
-    Serial.println(servo1Pos);
+    IrReceiver.resume(); 
   }
-  
-  if(abs(mapY) > padding)
-  {
-    servo2Pos = calcMove(mapY, servo2Pos);
-    servo2.write(servo2Pos);
-    
-    Serial.print("servo2Pos: ");
-    Serial.println(servo2Pos);
-  }
-  
-  if(false && abs(mapZ) > padding)
-  {
-    servo3Pos = calcMove(mapZ, servo3Pos);
-    servo3.write(servo3Pos);
-    
-    Serial.print("servo3Pos: ");
-    Serial.println(servo3Pos);
-  }
-  
-  if(false && !SW_state)
-  {
-    //servo4.write(servo4Pos);
-  }
-
-  delay(100);
 }
 
-int calcMove(int mapVar, int current)
+int calcMove(int toMove, int current)
 {
-    int steps = abs(mapVar)/10;
+    int steps = abs(toMove)/10;
     
-    if(mapVar > 0 && current < (180 - steps))
+    if(toMove > 0 && current < (180 - steps))
       return (current += steps); 
-    else if(mapVar < 0 && current > (0 + steps))
+    else if(toMove < 0 && current > (0 + steps))
       return (current -= steps); 
 }
