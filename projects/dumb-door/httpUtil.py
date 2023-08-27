fdcd39df57e5933a926a288afd165b11d1276a8b
Alexander <alethogar@protonmail.com>
2023-08-27 19:45:56 +0200

    Removed poor comments

--- projects/dumb-door/httpUtil.py
+++ projects/dumb-door/httpUtil.py
@@ -10,7 +10,7 @@ class HttpUtil:
     basicUserHeader = { "User-Agent": "Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0" }
     
     def getHtmlHeader(self, code: int, status: str) -> str:
-        # TODO, use ResponseStatus?
+        # TODO, use ResponseStatus
         
         return f"HTTP/1.0 {code} {status}\r\nContent-type: application/json\r\n\r\n"
     
@@ -18,6 +18,4 @@ class HttpUtil:
         # TODO, use class or obj as wrapper
         
         return f"{{ 'code': {code}, 'status': {status}, 'message': {message}, 'data': {data} }}"
-    
-    # TODO connecter wifi method
-    # TODO setup TCP socket method
+        
