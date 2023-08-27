9483d07e8b02b728632b944f748adfc8ab5cad09
Alexander <alethogar@protonmail.com>
2023-08-28 00:12:32 +0200

    Moved network, http out, fixed logger clashing in threads, cleaned imports/comments

--- projects/dumb-door/dumbDoor.py
+++ projects/dumb-door/dumbDoor.py
@@ -1,93 +1,73 @@
 import uasyncio
 import urequests
 import utime
-import uos
-import network
-import usocket
 import _thread
+import uos
 import micropython
 
 from picozero import RGBLED
 from machine import Pin, reset
+
 from queue import Queue  # https://github.com/peterhinch/micropython-async/blob/master/v3/primitives/queue.py
 
 import secrets # Secret values in secrets.py
 from logUtil import LogUtil
+from networkUtil import NetworkUtil
+from httpUtil import HttpUtil
 from rgbLedUtil import RgbLedUtil
 from rgbColor import RgbColor as rgb
 from lockStatus import LockStatus
 from lockAction import LockAction
 
 logFilename = "dumbDoor.log"
-logFileMaxSizeByte = int(512 * 1024) # 512 kb, capped to 2 mb on standard PICO. Keep in mind the temporary file will add additional when rotating/cleaning
+logFileMaxSizeByte = 3000#int(512 * 1024) # 512 kb, capped to 2 mb on standard PICO. Keep in mind the temporary file will add additional when rotating/cleaning
 
 rgbLed = RGBLED(red = 1, green = 2, blue = 3)
-button = Pin(4, Pin.IN, Pin.PULL_DOWN)
+mainButton = Pin(4, Pin.IN, Pin.PULL_DOWN)
 
 statusLed = RgbLedUtil(rgbLed)
 logger = LogUtil(logFilename, logFileMaxSizeByte)
+netUtil = NetworkUtil()
+httpUtil = HttpUtil()
 
 # UTC is preferable since it's near constant, local times, e.g. London, will not account for daylight saving time.
 datetimeSourceUrl = "http://worldtimeapi.org/api/timezone/etc/utc" # "http://worldtimeapi.org/api/timezone/Europe/London" 
 ledQueue = Queue()
 inputQueue = Queue()
 
-# TODO toggle lock/status button, motor, authentication on socket calls, modularize code (logging, led, network), ledutil some non-red colors = red
+# TODO toggle lock/status button, motor, authentication on socket calls, modularize code (logging, led, network, http stuff, motor/other outputs),
+# ledutil some non-red colors = red, logging error - wipes file
 
-async def connectWlan() -> str:
-    # Connect to WLAN using secrets from secrets.py file.
+def log(message: str, logToFile: bool = True, doPrint: bool = True, encoding: str = "utf-8") -> None:
+    # Log message thread safely.
+    
+    lock = _thread.allocate_lock()
+    with lock:
+        logger.log(message, logToFile, doPrint)
         
-    await logger.log("Connecting to WLAN...")
-    
-    wlanScan = []
-    statusLed.setColor(rgb.blue)
-    while not wlanScan:
-        wlan = network.WLAN(network.STA_IF)
-        wlan.active(True)
-        wlanScan = wlan.scan()
-        await logger.log(f"Scanned SSIDs: {[_[0] for _ in wlanScan]}")
-    
-    wlan.connect(secrets.ssid, secrets.password)
-    wlan.ifconfig(secrets.ipStruct)
-    while wlan.status() < 1:
-        await logger.log(f"Waiting for connection... Status: {wlan.status()}")
-        await statusLed.blink(rgb.blue)
-        
-    ip = wlan.ifconfig()[0]
-    await logger.log(f"Connected on IP: {ip}")
-    return ip
-
-async def setupSocketConnection(ip: str) -> Dict:
-    # Set up socket listeners for wireless calls.
-    
-    await logger.log("Setting up socket...")
-    socketAddress = usocket.getaddrinfo(ip, 80)[0][-1]
-    
-    listenerSocket = usocket.socket()
-    listenerSocket.setsockopt(usocket.SOL_SOCKET, usocket.SO_REUSEADDR, 1)
-    listenerSocket.bind(socketAddress)
-    listenerSocket.listen(1)
-    
-    await logger.log(f"Socket listening on {socketAddress}")
-    return listenerSocket
-
 async def setupLan() -> str:
-    # Connect to the internet using secrets from secrets.py file and set up .
+    # Connect to the internet using secrets from secrets.py file.
     
-    await logger.log("Connecting to LAN...")
+    log("Connecting to LAN...")
     
     defaultIp = "0.0.0.0"
     ip = defaultIp
     while ip == defaultIp:
-        await logger.log("Getting IP...")
-        ip = await connectWlan()
+        log("Getting IP...")
+        ip = netUtil.connectWlan(secrets.ssid, secrets.password, secrets.ipTuple)
         await statusLed.blinkOnce(rgb.blue, rgb.off)
         
+    log(f"Connected on IP: {ip}")
+    return ip
+
+async def setupDatetime() -> int:
+    # Fetch datetime and more basics for logging.
+    
     tickMsOffset = 0
     while not tickMsOffset:
         try:
-            await logger.log(f"Initializing datetime...")
-            headers = { "User-Agent": "Mozilla/5.0 (Windows NT 6.0; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0" }
+            log(f"Initializing datetime...")
+            headers = { "User-Agent": httpUtil.basicUserHeader }
             datetimeJson = urequests.get(datetimeSourceUrl, headers = headers).json()
             datetime = datetimeJson["datetime"]
             tickMsOffset = utime.ticks_ms()
@@ -95,13 +75,13 @@ async def setupLan() -> str:
             logger.datetimeInitiated = datetime
             logger.tickMsInitiated = tickMsOffset
             
-            await logger.log(f"datetime ({datetime}) and tickMsOffset ({tickMsOffset}) initialized")
+            log(f"datetime ({datetime}) and tickMsOffset ({tickMsOffset}) initialized")
         except Exception as e:
-            await logger.log("PICO connected to LAN, but there was an error with setting datetimeJson:")
-            await logger.log(str(e))
-            await statusLed.blinkOnce(rgb.blue, rgb.red) # Built-in wait, 1000 ms
+            log("PICO connected to LAN, but there was an error with setting datetimeJson:")
+            log(str(e))
+            statusLed.blinkOnce(rgb.blue, rgb.red) # Built-in wait, 1000 ms
 
-    return ip
+    return tickMsOffset
     
 async def toggleLockStatus(doorStatus: int, ledQueue: Queue) -> int:
     # Toggle lock status only, not the physical door lock. Updates LED: Green = locked, red = open.
@@ -116,16 +96,11 @@ async def toggleLockStatus(doorStatus: int, ledQueue: Queue) -> int:
         await ledQueue.put(rgb.green)
         await ledQueue.put(rgb.off)
     else:
-        await logger.log(f"toggleLockStatus - Invalid status: {newStatus}, defaulting to old status: {doorStatus}")
+        log(f"toggleLockStatus - Invalid status: {newStatus}, defaulting to old status: {doorStatus}")
         return doorStatus
     
-    await logger.log(f"Lock status updating: {doorStatus} -> {newStatus}")
+    log(f"Lock status updating: {doorStatus} -> {newStatus}")
     return newStatus
-    
-async def activateMotor(steps: int) -> int:
-    # Activate motor, rotating the shaft in direction and number of steps given by steps.
-    
-    return 0
     
 async def toggleLock(doorStatus: int, ledQueue: Queue) -> int:
     # Toggle lock, opening if status is locked, locking if status is open.
@@ -134,13 +109,13 @@ async def toggleLock(doorStatus: int, ledQueue: Queue) -> int:
     toggleBy = "unknown"
     newStatus = await toggleLockStatus(doorStatus, ledQueue)
     if(newStatus == LockStatus.locked):
-        await activateMotor(-100)
-        await logger.log(f"{toggleSource} - {toggleBy} - Locked")
+        #await activateMotor(-100)
+        log(f"{toggleSource} - {toggleBy} - Locked")
     elif(newStatus == LockStatus.unlocked):
-        await activateMotor(100)
-        await logger.log(f"{toggleSource} - {toggleBy} - Unlocked")
+        #await activateMotor(100)
+        log(f"{toggleSource} - {toggleBy} - Unlocked")
     else:
-        await logger.log(f"toggleLock - Invalid status: {newStatus}, defaulting to old status: {doorStatus}")
+        log(f"toggleLock - Invalid status: {newStatus}, defaulting to old status: {doorStatus}")
         return doorStatus
         
     return newStatus
@@ -148,6 +123,7 @@ async def toggleLock(doorStatus: int, ledQueue: Queue) -> int:
 def listenSocket(inputQueue: Queue, listenerSocket: Dict) -> None:
     # Listen and receive data over given paths on PICO IP.
     
+    connection = None
     while 1:
         try:
             connection, _ = listenerSocket.accept()
@@ -169,8 +145,8 @@ def listenSocket(inputQueue: Queue, listenerSocket: Dict) -> None:
                 status = "FAIL"
                 message = "Path was not valid"
             
-            connection.send(f"HTTP/1.0 {code} {status}\r\nContent-type: application/json\r\n\r\n")
-            connection.send(f"{{ 'code': {code}, 'status': {status}, 'message': {message}, 'data': {data} }}")
+            connection.send(httpUtil.getHtmlHeader(code, status))
+            connection.send(httpUtil.getJsonWrapper(code, status, message, data))
             
             if(action):
                 inputQueue.put_nowait(action)
@@ -186,9 +162,9 @@ async def listenMainButton(inputQueue: Queue) -> None:
     # Wait for main button input and determine actions to take.
     
     while 1:
-        last = button.value()
-        while(button.value() == 1) or (button.value() == last):
-            last = button.value()
+        last = mainButton.value()
+        while(mainButton.value() == 1) or (mainButton.value() == last):
+            last = mainButton.value()
             await uasyncio.sleep_ms(100)
             
         # TODO hardcoded lock only, missing status
@@ -210,7 +186,7 @@ async def mainLoop() -> None:
     await ledQueue.put(rgb.off)
     uasyncio.create_task(statusLed.blinkQueue(ledQueue, 200, 2000))
     
-    await logger.log("Main loop started")
+    log("Main loop started")
     while 1:
         if(not inputQueue.empty()):
             action = await inputQueue.get()
@@ -221,38 +197,39 @@ async def mainLoop() -> None:
             
         await uasyncio.sleep_ms(100)
         
-    await logger.log("Main loop completed")
-        
+    log("Main loop completed")
+    
 async def main() -> None:
     # Entry point
     
     try:
-        await logger.log("")
-        await logger.log("Initializing...")
-        await logger.log(f"Mem info: {str(micropython.mem_info())}")
-        await logger.log(f"Sys info: {str(uos.uname())}")
+        log("")
+        log("Initializing...")
+        log(f"Mem info: {str(micropython.mem_info())}")
+        log(f"Sys info: {str(uos.uname())}")
         
         ip = await setupLan()
-        listenerSocket = await setupSocketConnection(ip)
+        setupDatetime()
+        listenerSocket = netUtil.setupTcpSocketConnection(ip, port = 80, maxClients = 2)
         
-        await logger.log("Initialize complete")
+        log("Initialize complete")
         
         # Start main loop in a new thread and set up input listeners on main thread
         # Note: this is done because sockets seem to have issues when not on the first/main thread
         _thread.start_new_thread(runMainLoop, ())
         
-        await logger.log("Input listeners starting...")
+        log("Input listeners starting...")
         global inputQueue
         uasyncio.create_task(listenMainButton(inputQueue))
         uasyncio.create_task(listenSocket(inputQueue, listenerSocket))    
-        await logger.log("Input listeners started")
+        log("Input listeners started")
         
     except KeyboardInterrupt:
-        await logger.log("Keyboard interrupt")
+        log("Keyboard interrupt")
         reset()
     except Exception as e:
-        await logger.log("Error caught: ")
-        await logger.log(str(e))
+        log("Error caught: ")
+        log(str(e))
         raise e
         
 uasyncio.run(main())
