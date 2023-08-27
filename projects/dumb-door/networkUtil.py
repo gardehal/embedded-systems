aa0079d9d2e104b1e5baedd96e0ee1c1d24487d8
Alexander <alethogar@protonmail.com>
2023-08-27 19:46:07 +0200

    Fixed var ref bug

--- projects/dumb-door/networkUtil.py
+++ projects/dumb-door/networkUtil.py
@@ -5,7 +5,8 @@ class NetworkUtil:
     def connectWlan(self, ssid: str, password: str, ipTuple: tuple) -> str:
         # Connect to WLAN using secrets from secrets.py file.
         
-        wLanScan = []
+        wLan = None
+        wlanScan = []
         while not wlanScan:
             wlan = network.WLAN(network.STA_IF)
             wlan.active(True)
@@ -19,7 +20,7 @@ class NetworkUtil:
             
         return wlan.ifconfig()[0]
     
-    def setupTcpSocketConnection(self, ip: str, port: int = 80, maxClients: int = 1) -> tuple:
+    def setupTcpSocketConnection(self, ip: str, port: int = 80, maxClients: int = 4) -> tuple:
         # Set up a TCP socket for calls over networks.
         
         print("Setting up TCP socket...")
