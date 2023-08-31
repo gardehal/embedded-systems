1652b84c9cb9930078f93a1198f68215fd496cb3
Alexander <alethogar@protonmail.com>
2023-08-31 21:40:20 +0200

    Testing stepper motor hardware, seems one driver is fried and another only works with one coil at a time (test2, test3 code borrowed from other prople)

---
+++ projects/dumb-door/test.py
@@ -0,0 +1,12 @@
+import uasyncio
+from ioUtil import MotorUtil
+
+mainMotor = MotorUtil([6, 7, 8, 9])
+lmainMotor = MotorUtil([9, 8, 7, 6])
+
+#while 1: print("1")
+
+print("start")
+uasyncio.run(mainMotor.move(11))
+print("end")
+
---
+++ projects/dumb-door/test2.py
@@ -0,0 +1,27 @@
+# More details & schematic on electrocredible.com
+import utime
+from machine import Pin
+
+# Define the pins for the stepper motor
+stepper_pins = [Pin(12, Pin.OUT), Pin(13, Pin.OUT), Pin(14, Pin.OUT), Pin(15, Pin.OUT)]
+
+# Define the sequence of steps for the motor to take
+step_sequence = [
+    [1, 0, 0, 0],
+    [0, 1, 0, 0],
+    [0, 0, 1, 0],
+    [0, 0, 0, 1],
+]
+
+def step(direction, steps, delay):
+    global step_index
+    for i in range(steps):
+        step_index = (step_index + direction) % len(step_sequence)
+        for pin_index in range(len(stepper_pins)):
+            pin_value = step_sequence[step_index][pin_index] 
+            stepper_pins[pin_index].value(pin_value)
+        utime.sleep(delay)
+        
+step_index = 0
+step(1, 500, 0.1)
+#step(-1, 500, 0.5)
