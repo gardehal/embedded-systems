efdd2b6e6e640e22befbbe79790649346f4c2041
Alexander <alethogar@protonmail.com>
2023-09-05 22:51:11 +0200

    Working stepping code, though it squeeks

--- projects/dumb-door/test2.py
+++ projects/dumb-door/test2.py
@@ -1,31 +1,22 @@
-# More details & schematic on electrocredible.com
 import utime
 from machine import Pin
 
-# Define the pins for the stepper motor
 stepper_pins = [Pin(12, Pin.OUT), Pin(13, Pin.OUT), Pin(14, Pin.OUT), Pin(15, Pin.OUT)]
-
-# Define the sequence of steps for the motor to take
 step_sequence = [
-    [1, 0, 0, 1],
     [1, 1, 0, 0],
     [0, 1, 1, 0],
     [0, 0, 1, 1],
+    [1, 0, 0, 1],
 ]
 
 def step(direction, steps, delay):
-    global step_index
     for i in range(steps):
-        step_index = (step_index + direction) % len(step_sequence)
-        for j in range(len(stepper_pins)):
-            pin_value = step_sequence[step_index][j] 
-            stepper_pins[j].value(pin_value)
-        utime.sleep(delay)
-        
-        
-
+        for step in step_sequence:
+            for j in range(len(stepper_pins)):
+                stepper_pins[j].value(step[j])
+                utime.sleep(delay)
+                
 step_index = 0
 while 1:
     print("5 steps started")
-    step(1, 5, 0.5)
-#step(-1, 500, 0.5)
+    step(1, 5, 0.01)
