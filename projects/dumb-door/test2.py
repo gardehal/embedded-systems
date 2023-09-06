70775cde3db949e0544b20ce89cc86b103dfb34a
Alexander <alethogar@protonmail.com>
2023-09-06 23:22:59 +0200

    Code works with different stepper and controller, nema17 and driver not compatible? nema17 busted?

--- projects/dumb-door/test2.py
+++ projects/dumb-door/test2.py
@@ -1,8 +1,18 @@
 import utime
 from machine import Pin
 
-pins = [Pin(12, Pin.OUT), Pin(13, Pin.OUT), Pin(14, Pin.OUT), Pin(15, Pin.OUT)]
-stepSequence = [
+pins = [Pin(15, Pin.OUT), Pin(14, Pin.OUT), Pin(13, Pin.OUT), Pin(12, Pin.OUT)]
+halfstepSequence = [
+    [1, 0, 0, 0],
+    [1, 1, 0, 0],
+    [0, 1, 0, 0],
+    [0, 1, 1, 0],
+    [0, 0, 1, 0],
+    [0, 0, 1, 1],
+    [0, 0, 0, 1],
+    [1, 0, 0, 1],
+]
+fullstepSequence = [
     [1, 1, 0, 0],
     [0, 1, 1, 0],
     [0, 0, 1, 1],
@@ -11,21 +21,20 @@ stepSequence = [
 
 def step(direction, steps, delay):
     for _ in range(steps):
-        for step in stepSequence:
+        for step in halfstepSequence:
             for i in range(len(pins)):
                 pins[i].value(step[i])
-                #pins[i].value(step[i]) # Beeping caused by pins being on?
-                utime.sleep(delay)
+                utime.sleep_ms(delay)
                 
 step_index = 0
 
 if(1):
     while 1:
         print("5 steps started")
-        step(1, 5, 0.05)
+        step(1, 5, 5)
 else:
     print("5 steps started")
-    step(1, 5, 0.05)
+    step(1, 5, 5)
     
 for i in range(len(pins)):
     pins[i].value(0)
